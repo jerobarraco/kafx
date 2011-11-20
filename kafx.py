1fc5151665bf3b521f01d12a355d1be5554550e0
nande <devnull@localhost>
2011-11-20 04:33:17 +0000

    pequeña pulida aunque no quedé conforme con como carga el ui

--- kafx.py
+++ kafx.py
@@ -1,27 +1,37 @@
-# -*- coding: utf-8 -*-
 from PySide import QtCore, QtGui, QtUiTools, phonon
 import sys
 
+class Ventana():
+	def __init__(self):
+		loader = QtUiTools.QUiLoader() 
+		self.ui = loader.load("gui.ui")
+		#self.ui.setupUi(self)
+		#QtCore.QMetaObject.connectSlotsByName(ui)
+		self.ui.pushButton.clicked.connect(self.vidbutton)
+		self.ui.pushButton_2.clicked.connect(self.fxbutton)
+		self.ui.pushButton_3.clicked.connect(self.assbutton)
+		self.ui.pushButton_4.clicked.connect(self.encbutton)
+	def show(self):
+		self.ui.show()
+                
+	@QtCore.Slot()
+	def vidbutton(self):
+		self.vidfile=QtGui.QFileDialog.getOpenFileName(self.ui,"Abrir archivo de video", "test/*.mkv", "Archivos de video")    
+		
+	@QtCore.Slot()
+	def fxbutton(self):
+		self.fxfile=QtGui.QFileDialog.getOpenFileName(self.ui,"Abrir archivo de efectos", "test/*.py", "Archivos de efectos")    
+	@QtCore.Slot()
+	def assbutton(self):
+		self.assfile=QtGui.QFileDialog.getOpenFileName(self.ui,"Abrir archivo de subtitulos", "test/*.ass", "Archivos de subtitulos")    
+		
+	@QtCore.Slot()
+	def encbutton(self):
+		print "Encoding!!"  
+		
+		
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    ff = QtCore.QFile("gui.ui")
-    ff.open(QtCore.QFile.ReadOnly)
-    mw = QtUiTools.QUiLoader().load(ff)
-    ff.close()
-    @QtCore.Slot()
-    def vidbutton():
-        vidfile=QtGui.QFileDialog.getOpenFileName(mw,"Abrir archivo de vídeo", "test/*.mkv", "Archivos de vídeo")    
-    def fxbutton():
-        fxfile=QtGui.QFileDialog.getOpenFileName(mw,"Abrir archivo de efectos", "test/*.py", "Archivos de efectos")    
-    def assbutton():
-        assfile=QtGui.QFileDialog.getOpenFileName(mw,"Abrir archivo de subtítulos", "test/*.ass", "Archivos de subtítulos")    
-    def encbutton():
-        print "Encoding!!"  
-    mw.pushButton.clicked.connect(vidbutton)
-    mw.pushButton_2.clicked.connect(fxbutton)
-    mw.pushButton_3.clicked.connect(assbutton)
-    mw.pushButton_4.clicked.connect(encbutton)
-    mw.show()
-    #myapp = MyMainWindow()
-    #myapp.show()
-    sys.exit(app.exec_())
+	app = QtGui.QApplication(sys.argv)
+	vent = Ventana()
+	vent.show()
+	sys.exit(app.exec_())
