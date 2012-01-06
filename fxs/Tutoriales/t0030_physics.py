efef20f04dd1a4a4d4333eade5c74e1426a2c5a5
nande <devnull@localhost>
2012-01-06 22:17:55 +0000

    TErminado el tema del engine de physics puedo agregarle mas pero por ahora no

--- fxs/Tutoriales/t0030_physics.py
+++ fxs/Tutoriales/t0030_physics.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 from libs import comun, physics
 from libs.draw import extra
-				
-t1 = extra.CargarTextura("texturas/T_Negro2.png")				
+
+t1 = extra.CargarTextura("texturas/T_Negro2.png")
 world = None #globales on son buena idea, alentan todo y es mas dificil saber que estas haciendo
 objs = []
 objs2 = []
-class Efecto():		
+class Efecto():
 	def EnSilabaInicia(self, sil):
-		parts = sil.CrearParticulas(t1, escala=0.4 ) 
-		sil.parts = [parts[pos] for pos in xrange(0, len(parts), 5) ] #tomamos 1 cada 100 parts
+		parts = sil.CrearParticulas(t1, escala=0.5 )
+		sil.parts = [parts[pos] for pos in xrange(0, len(parts), 10) ] #tomamos 1 cada 100 parts
 		sil.moving = False
 
 	def EnSilabaDorm(self, sil):
 		sil.PaintWithCache()
-		
+
 	def EnSilaba(self, sil):
 		global world, objs
 		if not sil.moving:
@@ -22,31 +22,49 @@ class Efecto():
 			for p in sil.parts:
 				world.CreateSprite(p)
 				objs.append(p)
-				
+
 class Efecto2():
 	def EnSilabaInicia(self, s):
 		s.nueva= True
-	def EnDialogo(self, d):
-		d.PaintWithCache()
-		
+
 	def EnSilaba(self, s):
-		global world,objs2
-		if s.nueva:			
+		global world, objs2
+		if s.nueva:
 			s.nueva= False
-			world.CreateVector(s)
+			world.CreateVector(s, True, True)
 			objs2.append(s)
 			s.original.color1.CopyFrom(s.original.color2)
-	
-		
-			
+
+	def EnDialogo(self, d):
+		d.PaintWithCache()
+
+class Efecto3():
+	def EnDialogoInicia(self, d):
+		d.nueva = True
+		d.creada = False
+
+	def EnDialogo(self, d):
+		if d.nueva:
+			global world
+			d.nueva = False
+			world.CreateVector(d, False, True)
+			d.creada = True
+		d.PaintWithCache()
+
+	def EnDialogoSale(self, d):
+		if d.creada:
+			d.creada = False
+			world.DestroyVector(d)
+
+
 class FxsGroup(comun.FxsGroup):
 	def __init__(self):
 		global world
-		self.fxs = (Efecto(), Efecto2(), Efecto2())
+		self.fxs = (Efecto(), Efecto2(), Efecto3())
 		#no puedo crear dos efecto() porque intentaria crear dos mundos
 		self.saltar_cuadros = False
 		world = physics.World()
-				
+
 	def EnCuadroFin(self):
 		global world, objs,objs2
 		world.Update(True)
@@ -54,12 +72,12 @@ class FxsGroup(comun.FxsGroup):
 			o.Paint()
 			o.color.a -= 0.01
 			if o.color.a <0.0:
-				world.Destroy(o)
+				world.DestroySprite(o)
 				objs.remove(o)
-				
+
 		for o in objs2[:]:#[:]es para poder hacer remove
 			o.Paint()
 			o.original.color1.a -= 0.01
 			if o.original.color1.a <0.0:
-				world.Destroy(o)
+				world.DestroyVector(o)
 				objs2.remove(o)
