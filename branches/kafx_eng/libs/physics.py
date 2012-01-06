198e8852fca33858ea2169ba51db91afd9599617
nande <devnull@localhost>
2012-01-06 22:17:55 +0000

    TErminado el tema del engine de physics puedo agregarle mas pero por ahora no

--- branches/kafx_eng/libs/physics.py
+++ branches/kafx_eng/libs/physics.py
@@ -6,8 +6,8 @@ Created on Thu Jan 05 17:29:45 2012
 """
 import pymunk as pm
 from libs import video
-from math import isnan, isinf
-	  
+from math import isnan, isinf, pi
+
 class World():
 	def __init__(self, grav_x=0, grav_y=500.0, ground=True):
 		self.__dt = 1.0/video.vi.fps
@@ -15,7 +15,7 @@ class World():
 		self.maxy = video.vi.height
 		self.sprites = []
 		self.vectors = []
-			
+
 		self.hx = self.maxx/2.0
 		self.hy = self.maxy/2.0#unused
 		self.space = pm.Space() #2
@@ -28,7 +28,7 @@ class World():
 			#l3 = pm.Segment(body, (self.maxx, 0.0), (self.maxx, -self.maxy), 5.0)#right
 			#self.space.add_static(l1,l2,l3) # 3
 			self.space.add_static(l1)
-			
+
 	def Update(self, updateObjs=True):
 		self.space.step(self.__dt)
 		if updateObjs:
@@ -36,16 +36,35 @@ class World():
 				UpdateSprite(sp)
 			for vec in self.vectors:
 				UpdateVector(vec)
-			
-	def Destroy(self, obj):
+
+	def __Destroy(self, obj):
 		if obj._dynamic: self.space.remove(obj.shape.body)
-		self.space.remove(obj.shape)		
-		if obj in self.vectors: self.vectors.remove(obj)
-		if obj in self.sprites: self.sprites.remove(obj)
+		self.space.remove(obj.shape)
 		del obj.shape
-		
-	def CreateBody(self, pos_x, pos_y, width, height, dynamic = True, square=False):
-		mass = 1.0			
+
+	def Destroy(self, obj):
+		#Slower but easier to use
+		if obj in self.vectors:
+			self.DestroyVector(obj)
+		elif obj in self.sprites:
+			self.DestroySprite(obj)
+		else:
+			try:
+				self.__Destroy(obj)
+			except:
+				print "quieren destruir un objeto que no existe"
+
+
+	def DestroySprite(self, obj):
+		self.sprites.remove(obj)
+		self.__Destroy(obj)
+
+	def DestroyVector(self, obj):
+		self.vectors.remove(obj)
+		self.__Destroy(obj)
+
+	def CreateBody(self, pos_x, pos_y, width, height, angle = 0.0, dynamic = True, square=False):
+		mass = 1.0
 		if dynamic:
 			if square:
 				inertia = pm.moment_for_box(mass, width*2, height*2)
@@ -54,57 +73,61 @@ class World():
 			body = pm.Body(mass, inertia) # 2
 		else:
 			body = pm.Body()
-			
+
+		body.angle = angle #the angle can only be set before adding to space
+
 		if square:
-			verts= ( (-height, -width), (-height, width), (height, width), (height, -width))
+			verts = ( (-height, -width), (-height, width), (height, width), (height, -width))
 			shape = pm.Poly(body, verts )
-		else:			
+		else:
 			shape = pm.Circle(body, width) # 4
-			
+
 		body.position = pos_x, pos_y
-			
+
 		self.space.add(shape)
 		if dynamic:
 			self.space.add(body) # 5
 		return shape
-		
+
 	def CreateVector(self, vector, dynamic = True, square=False):
 		vector._dynamic = dynamic
 		a = vector.actual
-		
+
 		x = a.pos_x+a.org_x
-		y = a.pos_y-a.org_y
+		y = a.pos_y+a.org_y
 		w = vector.original._ancho /2.0
 		h = vector.original._alto /2.0
-		vector.shape = self.CreateBody( x, y, w, h, dynamic, square)
-		
+		vector.shape = self.CreateBody( x, y, w, h, a.angle, dynamic, square)
+
 		self.vectors.append(vector)
-		
+
 	def CreateSprite(self, sprite, dynamic = True, square=False):
 		sprite._dynamic = dynamic
-		sprite.shape = self.CreateBody(
-			sprite.x+sprite.org_x, sprite.y+sprite.org_y,
-			sprite._ancho*(1.0/sprite.scale_x) /2.0,
-			sprite._alto*(1.0/sprite.scale_y) /2.0, dynamic, square)
+		x= sprite.x+sprite.org_x
+		y= sprite.y+sprite.org_y
+		w=sprite._ancho*(1.0/sprite.scale_x) /2.0
+		h= sprite._alto*(1.0/sprite.scale_y) /2.0
+
+		sprite.shape = self.CreateBody(x, y, w, h, sprite.angle, dynamic, square)
 		self.sprites.append(sprite)
-		
+
 def UpdateVector(vector):
-	body = vector.shape.body 
+	body = vector.shape.body
 	pos = body.position
 
 	a = vector.actual
-	a.angle = body.angle
+	a.angle = body.angle #+ vector.original.angle #hasta que pymunk me tome bien el ángulo
 	a.pos_x = pos.x - a.org_x
-	a.pos_y = pos.y + a.org_y
+	a.pos_y = pos.y - a.org_y
 	#pongo el isinf 1º porque le pymunk me tira ese
 	if isinf(a.angle) or isnan(a.angle) : a.angle = 0.0
-	
+
 def UpdateSprite(sprite):
 	body = sprite.shape.body
 	pos = body.position
 	sprite.x = pos.x - sprite.org_x
 	sprite.y = pos.y - sprite.org_y
 	sprite.angle = body.angle
-	
+
 	if isinf(sprite.angle) or  isnan(sprite.angle) : sprite.angle =0.0
-	
+
