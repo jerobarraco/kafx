09829c64bdb3a875ca82cbd36cf51ba658ed45d3
nande <devnull@localhost>
2012-01-19 17:29:15 +0000

    un par de efectos traducidos

--- branches/kafx/fxs/not_so_simple.py
+++ branches/kafx/fxs/not_so_simple.py
@@ -4,32 +4,32 @@ from math import pi, sin
 
 class FX1(comun.Fx):
 		#Entrada: sea por silabas y con fadein
-		def EnSilabaEntra(self, s):
+		def OnDialogueIn(self, s):
 			s.Fade(0, 1)#fadein
-			s.Pintar()
+			s.Paint()
 
 		#Estatico: solo color
 		#Las silabas que no son animadas las pintamos aparte
-		def EnSilabaDorm(self, s):
-			s.Pintar() #Tal cual como vienen
+		def OnSyllableSleep(self, s):
+			s.Paint() #Tal cual como vienen
 
 		#FX Sil: un escalado a 150 y que vuelva a 100
 		#Las que se estan animando
-		def EnSilaba(self, s):
+		def OnSyllable(self, s):
 			#La scale está dada por scale_x y scale_y, 1 = 100% , 0.5 = 50% , 1.5 = 150% , 2 = 200% se entiende? (-1 es como dado vuelta)
 			#para que crezca y decrezca usaré la funcion sin, q vos le das un angulo entre 0 y 2*pi y va de 0 a 1 y de nuevo a 0 de forma bastante suave. (es un circulo)
-			s.actual.scale_x = s.actual.scale_y = 1.5 * sin(2*pi*s.progreso)
-			s.actual.color1.CopiarDe(s.original.color2)#Copiamos el color del color secundario
-			s.Pintar()
+			s.actual.scale_x = s.actual.scale_y = 1.5 * sin(2*pi*s.progress)
+			s.actual.color1.CopyFrom(s.original.color2)#Copiamos el color del color secundario
+			s.Paint()
 		#Las que ya fueron animadas las pintamos con el otro color
-		def EnSilabaMuerta(self, s):
-			s.actual.color1.CopiarDe(s.original.color2)#Copiamos el color del color secundario
-			s.Pintar()
+		def OnSyllableDead(self, s):
+			s.actual.color1.CopyFrom(s.original.color2)#Copiamos el color del color secundario
+			s.Paint()
 
 		#Salida: un fadeout x silaba
-		def EnSilabaSale(self, s):
+		def OnSyllableOut(self, s):
 			s.Fade(1, 0)#fadeout
-			s.Pintar()
+			s.Paint()
 
 
 class FxsGroup(comun.FxsGroup):
---
+++ branches/kafx/fxs/tutoriales/t0003simple_chain.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Hecho por Abelkm
+http://my.opera.com/Abelkm/blog/
+vil copia del tuto de nande! pero a mi estilo xD
+"""
+
+
+from libs import comun
+
+class FX1(comun.Fx):
+
+	def DialogoQueEntra(self, s, p):
+		#acá se ponen todos los efectos de la entrada del diálogo
+		s.progress = p
+		s.Fade(0, 1)
+		s.Paint()
+
+	def OnDialogueIn(self, d):
+		#acá se encadena y se le da una duración para cada sílaba entrante, así no se la de el kafx automáticamente
+		d.Chain(self.DialogoQueEntra, duration=150)
+
+
+
+	def DialogoQueSale(self, s, p):
+		#lo mismo que antes, efectos
+		s.progress = p
+		s.Fade(1, 0)
+		s.Paint()
+
+	def OnDialogueOut(self, d):
+		#acá también es lo mismo, encadenar y poner tiempo, sólo que con la salida del diálogo
+		d.Chain(self.DialogoQueSale, duration=40)
+
+
+	def OnDialogue(self, d):
+		#diálogo for the lulz
+		d.Paint()
+
+
+
+class FxsGroup(comun.FxsGroup):
+	def __init__(self):
+		self.fxs = (FX1(),)
+		self.in_ms = 300 #duración entrada del diálogo
+		self.out_ms = 300 #duración salida del diálogo
+
