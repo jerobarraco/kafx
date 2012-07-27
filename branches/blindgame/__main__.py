a6db7b886dfea1e799c4e02dd6c7f80d9b3289e5
nande <devnull@localhost>
2012-07-27 23:36:20 +0000

    blindgame

---
+++ branches/blindgame/__main__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+print "========BLIND GAME========="
+print" ... "
+raw_input("")
+print " ...... "
+raw_input("")
+print " ... Te despiertas en medio de la madrugada,"
+raw_input("")
+print " no sabes por qué, pero hay algo que te molesta."
+raw_input("")
+print " Te levantas para acercarte a la ventana de tu habitación."
+raw_input("")
+raw_input(" Abres la ventana.")
+raw_input("")
+raw_input(" En ese momento sientes una fría brisa que te recuerda que todavía es invierno.")
+raw_input("")
+raw_input(" Miras hacia arriba, pero lo único que se ve es la luna, brillando solitaria.")
+raw_input("")
+raw_input(" \"...\" ")
+raw_input("")
+raw_input(" Bajas la vista, ves la calle y las casas vecinas; nada fuera de lugar.")
+raw_input("")
+raw_input(" O eso creías, hasta que ves \"algo\" cerca de la case de los vecinos de enfrente.")
+raw_input("")
+raw_input(" Fue fugaz, pero estás casi seguro que has visto algo.")
+raw_input("")
+print "¿Vas a ver qué fue eso?: Y / N"
+choise1 =raw_input("")
+if choise1 == "Y" or "y" or "Yes" or "yes":
+	print "..."
+	raw_input(" No. Algo no está bien.")
+	raw_input("")
+	raw_input("")
+
+elif choise1 == "N" or "n" or "No" or "no":
+	print "......"
+	raw_input(" ... \"Haa...\" -- Se te escapa un suspiro.")
+	raw_input("")
+	raw_input(" \"No sé qué estaba esperando que sucediese...\" -- Te preguntas en voz alta.")
+	raw_input("")
+	raw_input(" \"...\" ")
+	raw_input("")
+	raw_input("Decides cerrar la ventana y volver a la cama para intentar dormirte de nuevo.")
+	raw_input("")
+	print "BAD END"
+	raw_input("")
+	raw_input("")
+	raw_input("")
